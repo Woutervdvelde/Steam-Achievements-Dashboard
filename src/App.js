41cf035c1589f4b6987785dc70344a8901d09e5f
Wouter <woutervandervelde2000@hotmail.com>
2021-05-07 21:27:45 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/SetupPopup'
+import React from "react";
+import SetupPopup from "./components/SetupPopup";
+
+function App() {
+
+  return (
+    <div className="App">
+      <SetupPopup/>
+    </div>
+  );
+}
+
+export default App;
