9e1d79133a5eed0e06aa63d27b4c66dcb6550435
Wouter <woutervandervelde2000@hotmail.com>
2021-05-08 21:33:55 +0200

    added form for userid and apikey, validation half way done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import './components/SetupPopup'
 import React from "react";
+import { useCookies } from 'react-cookie';
+
+
 import SetupPopup from "./components/SetupPopup";
 
 function App() {
-
+    const [cookies, setCookie, removeCookie] = useCookies(['api_key', 'user_id', 'api_key_valid', 'user_id_valid']);
+    if (cookies.api_key_valid && cookies.user_id_valid) {
+        return (
+            <div className="App">
+                <SetupPopup/>
+            </div>
+        )
+    }
   return (
     <div className="App">
       <SetupPopup/>
--- src/components/SetupPopup.js
+++ src/components/SetupPopup.js
@@ -1,71 +1,114 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default function SetupPopup() {
-    const [showModal, setShowModal] = React.useState(false);
-    return (
-        <>
-            <button
-                className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(true)}
-            >
-                Open regular modal
-            </button>
-            {showModal ? (
-                <>
-                    <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                    >
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                            {/*content*/}
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                {/*header*/}
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                                    <h3 className="text-3xl font-semibold">
-                                        Modal Title
-                                    </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                                    </button>
-                                </div>
-                                {/*body*/}
-                                <div className="relative p-6 flex-auto">
-                                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                                        I always felt like I could do anything. That’s the main
-                                        thing people are controlled by! Thoughts- their perception
-                                        of themselves! They're slowed down by their perception of
-                                        themselves. If you're taught you can’t do anything, you
-                                        won’t do anything. I was taught I could do everything.
-                                    </p>
-                                </div>
-                                {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                                    <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Close
-                                    </button>
-                                    <button
-                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Save Changes
-                                    </button>
-                                </div>
+export default class SetupPopup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user_id: '',
+            api_key: '',
+            button_loading: false,
+            errors: {
+                user_id: '',
+                api_key: ''
+            }
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        this.setState({
+            button_loading: true,
+            errors: {
+                user_id: '',
+                api_key: ''
+            }
+        })
+        let err = false;
+
+        if (this.state.user_id === '') {
+            err = true;
+            this.state.errors.user_id = "No user ID provided";
+        }
+
+        if (this.state.api_key === '') {
+            err = true;
+            this.state.errors.api_key = "No API key provided";
+        }
+
+
+        if (err)
+            return this.setState({button_loading: false});
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <>
+                <div
+                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50"
+                >
+                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                        {/*content*/}
+                        <div
+                            className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white">
+                            {/*header*/}
+                            <div
+                                className="flex items-start justify-between p-5 border-b">
+                                <h3 className="text-3xl font-semibold">
+                                    Your info
+                                </h3>
+                            </div>
+                            {/*body*/}
+                            <div className="relative p-6 flex-auto">
+                                <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                                    To be able to see your stats you need to fill in your user id and also an API key.
+                                    You can see how you obtain an API key <a
+                                    href="https://steamcommunity.com/dev/apikey" className="underline">here</a>
+                                </p>
+                                <form>
+                                    <label htmlFor="user_id">User id:</label>
+                                    <input name="user_id" type="text" placeholder="76561198201679638"
+                                           onChange={this.handleChange} className="mx-2 border-dashed"/>
+                                    <span className='error'>{this.state.errors.user_id}</span>
+                                    <br/>
+                                    <label htmlFor="api_key"> Api key:</label>
+                                    <input name="api_key" type="text" placeholder="AAAAABBBBBBCCCCCDDDDD11111222233"
+                                           onChange={this.handleChange} className="mx-2"/>
+                                    <span className='error'>{this.state.errors.api_key}</span>
+                                </form>
+                            </div>
+                            {/*footer*/}
+                            <div
+                                className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                <button type="button" onClick={this.handleSubmit}
+                                        className="py-2 px-4 w-full sm:w-1/6 rounded-md bg-black text-white font-bold shadow hover:shadow-lg ">
+                                    {this.state.button_loading ?
+                                        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                                    strokeWidth="4"/>
+                                            <path className="opacity-75" fill="currentColor"
+                                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+                                        </svg> : "Save"}
+                                </button>
                             </div>
                         </div>
                     </div>
-                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-                </>
-            ) : null}
-        </>
-    );
+                </div>
+                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>);
+    }
 }
